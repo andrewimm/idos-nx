a909f2af0a73174c3c3f12fabe1b1136dd4c180f
Andrew Imm <aimm22@gmail.com>
2023-01-01 23:01:30 -0800

    Zero out BSS using labels from the linker

--- kernel/src/init.rs
+++ kernel/src/init.rs
@@ -1,6 +1,19 @@
 
+extern {
+    #[link_name = "__bss_start"]
+    static mut label_bss_start: u8;
+    #[link_name = "__bss_end"]
+    static label_bss_end: u8;
+}
+
 /// Zero out the .bss section. Code may assume this area starts as zeroes.
 pub unsafe fn zero_bss() {
+    let bss_start = &mut label_bss_start as *mut u8;
+    let bss_length = (&label_bss_end as *const u8 as usize) - (bss_start as usize); 
+    let bss_slice = core::slice::from_raw_parts_mut(bss_start, bss_length);
+    for i in 0..bss_slice.len() {
+        bss_slice[i] = 0;
+    }
 }
 
 /// Initialize the GDT, IDT
