ffa8ba8307fbb187c6c753469d597df5a33b8ae0
Andrew Imm <aimm22@gmail.com>
2023-06-29 09:58:54 -0700

    Use new task fields for better panic info

--- kernel/src/panic.rs
+++ kernel/src/panic.rs
@@ -1,7 +1,26 @@
 use core::panic::PanicInfo;
 
 #[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    crate::kprint!("{}\n", info);
+extern "C" fn panic(info: &PanicInfo) -> ! {
+    let id = crate::task::switching::get_current_id();
+    crate::kprint!("Kernel Panic in {:?} ", id);
+    print_name(id);
+    crate::kprint!("\n  {}\n", info);
     loop {}
 }
+
+#[inline(never)]
+fn print_name(id: crate::task::id::TaskID) {
+    match crate::task::switching::get_task(id) {
+        Some(lock) => match lock.try_read() {
+            Some(task) => {
+                crate::kprint!("({})", task.filename.as_str());
+                return;
+            },
+            _ => (),
+        },
+        _ => (),
+    }
+    crate::kprint!("(UNKNOWN)");
+}
+
