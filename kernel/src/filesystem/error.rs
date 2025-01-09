62ed6c1c3057dd8a2688f2239063760c535d13f2
Andrew Imm <aimm22@gmail.com>
2025-01-08 21:30:39 -0800

    Legacy filesystem stack is completely gone

--- kernel/src/filesystem/error.rs
+++
@@ -1,7 +0,0 @@
-#[derive(Debug)]
-pub enum FsError {
-    /// Indicates that a drive does not exist, or was uninstalled
-    DriveNotFound,
-    /// An attempt to install a FS or device driver failed
-    InstallFailed,
-}
